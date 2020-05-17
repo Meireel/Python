2d044e126199cf020b529fa8c67ed7dafd2cfc15
Meireel <meireel96@gmail.com>
2020-05-17 16:00:51 +0300

    Пора года

--- HW_1.py
+++ HW_1.py
@@ -1,8 +1,17 @@
-month = 1
 
-if x == 1:
+a=int(input('Введите номер месяца: \n'))
+
+if a in [1, 2, 12]:
     print('Winter')
 
+elif a in [3, 4, 5]:
+        print('Spring')
+
+elif a in [6, 7, 8]:
+        print('Summer')
+
+elif a in [9, 10, 11]:
+        print('Autumn')
 
 else:
-        print:('Введите номер месяца: \n')
+    print('Введите правильный номер')
